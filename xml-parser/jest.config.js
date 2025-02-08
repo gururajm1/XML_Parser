c72796820c0578043e4909272ea75a766d79a65e
gururajm1 <guruguru6631@gmail.com>
2025-02-08 15:35:39 +0530

    commited

--- xml-parser/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom', // For DOM testing
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // Add custom matchers
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    transform: {
-      '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript files
-    },
-    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'], // Look for test files
-  };
